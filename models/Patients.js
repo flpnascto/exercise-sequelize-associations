72b9f43319bad31f15594add070418037b9b1ed0
Felipe Nascimento <flpnascto@gmail.com>
2021-05-20 18:28:20 -0300

    criar model Patients

---
+++ models/Patients.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const Patient = sequelize.define('Patient',
+    {
+      patient_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      fullname: DataTypes.STRING,
+      plan_id: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+    },
+    {
+      timestamps: false,
+      tableName: 'Patients',
+      underscored: true,
+    }
+  );
+
+  Patient.associate = (models) => {
+    Patient.belongsTo(models.Plan,
+      { foreignKey: 'plan_id', as: 'plan' })
+  };
+
+  return Patient;
+};
