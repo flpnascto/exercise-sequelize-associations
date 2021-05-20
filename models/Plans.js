5513884f108641f3bf74e6492c465aa745ce8437
Felipe Nascimento <flpnascto@gmail.com>
2021-05-20 18:28:02 -0300

    criar model Plans

---
+++ models/Plans.js
@@ -0,0 +1,27 @@
+const definePlans = (sequelize, DataTypes) => {
+  const Plan = sequelize.define('Plan',
+    {
+      plan_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      coverage: DataTypes.STRING,
+      price: DataTypes.DOUBLE,
+    },
+    {
+      timestamps: false,
+      tableName: 'Plans',
+      underscored: true,
+    }
+  );
+
+  Plan.associate = (models) => {
+    Plan.hasMany(models.Patient,
+      { foreignKey: 'plan_id', as: 'patients' })
+  }
+
+  return Plan;
+};
+
+module.exports = definePlans;
